41051446f03cdea8c32f50e5cde3d02525bffd57
tolseone <asemisynov@mail.ru>
2023-05-30 19:03:07 +0300

    completed project, but command for delete not work

---
+++ project_1_phonebook/controller.py
@@ -0,0 +1,50 @@
+import view, model
+
+def start():
+    view.welcome()
+    username = input("Как мне к вам обращаться?\n")
+    while True:
+        view.сontacting_the_user(username)
+        view.menu()
+        answer = input("Введите команду: ")
+        if answer == "1":
+            date = model.get_data()
+            view.show_contacts(date)
+        elif answer == "2":
+            contact = input("Для того, чтобы создать новый контакт, введите\nФ И О и номер телефона через пробел: ")
+            res = model.add_contact(contact)
+            if res:
+                view.succes(res)
+            else:
+                view.not_succes()
+        elif answer == "3":
+            info = input("Введите данные контакта для поиска: ")
+            result = model.search(info)
+            view.show_contacts(result)
+        elif answer == "4":
+            while True:
+                view.submenu()
+                user_response = input("Введите команду: ")
+                if user_response == "1":
+                    old_name = input("Введите данные, которые хотите заменить(в формате Ф И О номер телефона): ")
+                    new_name = input("Введите новые данные для замены: ")
+                    old_data, new_data = model.replace_data(old_name, new_name)
+                    view.show_red_contacts(old_data, new_data)
+                elif user_response == "2":
+                    date = model.get_data()
+                    view.show_contacts(date)
+                elif user_response == "3":
+                    break
+                else:
+                    view.error()
+        elif answer == "5":
+            info_for_delete = input("Введите Имя и номер телефона контакта через пробел, который вы хотите удалить: ").split()
+            deleted_cont = model.delete_contact(info_for_delete)
+            if isinstance(deleted_cont, str):
+                view.deleted_contact(deleted_cont)
+            else:
+                view.error_to_del_contact()
+        elif answer == "6":
+            break
+        else:
+            view.error()
---
+++ project_1_phonebook/view.py
@@ -0,0 +1,47 @@
+def welcome():
+    print("Приветствую, Пользователь!", end=' ')
+
+def сontacting_the_user(username: str):
+    print(f'----------------------------\n{username.capitalize()}, что для вас сделать?')
+
+def menu():
+    print(
+        '1 - Показать все контакты\n'
+        '2 - Добавить контакт\n'
+        '3 - Найти контакт\n'
+        '4 - Отредактировать контакт\n'
+        '5 - Удалить контакт\n'
+        '6 - Выход'
+    )
+
+def error():
+    print("Ошибка, введённой команды не существует.")
+
+def show_contacts(date: list):
+    print(*date, sep='\n')
+
+def succes(res):
+    print(f'Контакт создан успешно!\n{res}')
+
+def not_succes():
+    print("Контакт не создан. Вы не ввели данные.")
+
+def submenu():
+    print(
+        '1 - Изменить Фамилию, Имя, Отчество, Телефон\n'
+        '2 - Вывести список всех контактов\n'
+        '3 - Вернуться в основное меню'
+    )
+
+def show_red_contacts(old: list, new: list):
+    print("Контакты: ", *old, sep='\n')
+    print("Заменены на: ",*new, sep='\n')
+
+def deleted_contact(deleted_contact: str):
+    print(f'Успешное удаление контакта:\n{deleted_contact}')
+
+def error_to_del_contact():
+    print("Ошибка! контакта с такими данными не существует.")
+
+# if __name__ == "__main__":
+#     menu()
