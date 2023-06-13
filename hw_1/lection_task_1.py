401c0eeb0751fb2c6b84e2277469c1a13268b440
tolseone <asemisynov@mail.ru>
2023-06-13 22:13:39 +0300

    FOR DIPLOME

--- hw_1/lection_task_1.py
+++ hw_1/lection_task_1.py
@@ -1,14 +1,4 @@
-# В списке хранятся числа, вывести список четных чисел и их квадраты
-
-list_1 = [1, 2, 3, 5, 8, 15, 23, 38]
-new_list = [(eto_chislo, eto_chislo ** 2) for eto_chislo in list_1 if eto_chislo % 2 == 0]
-print(new_list)
-
-
-
-
-
-
+print(input().split())
 
 
 
